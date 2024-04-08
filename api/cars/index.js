7360411253e808aba31cc14f0cb86de2a8d0746f
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com>
2024-04-08 14:50:27 +0000

    added cars fucntion

---
+++ api/cars/index.js
@@ -0,0 +1,54 @@
+module.exports = async function (context, req) {
+    const express = require('express');
+const app = express();
+
+
+
+app.use(express.json());
+
+const cars = require('./cars.json');
+
+//get all cars
+app.get('/cars', (req, res) => {
+    res.json(cars);
+});
+
+//get car by id
+app.get('/cars/:id', (req, res) => {
+    const id = req.params.id;
+    const car = cars.find(car => car.id === id);
+    res.json(car);
+});
+
+//update car
+app.put('/cars/:id', (req, res) => {
+    const id = req.params.id;
+    const updatedCar = req.body;
+    const index = cars.findIndex(car => car.id === id);
+    cars[index] = updatedCar;
+    res.json(updatedCar);
+});
+
+//delete car
+app.delete('/cars/:id', (req, res) => {
+    const id = req.params.id;
+    const index = cars.findIndex(car => car.id === id);
+    cars.splice(index, 1);
+    res.json({ message: `Car with id ${id} deleted` });
+});
+
+//add car
+app.post('/cars', (req, res) => {
+    console.log(req);
+    const newCar = req.body;
+    console.log(newCar);
+    cars.push(newCar);
+    res.json(newCar);
+});
+
+//start app at localhost:3001
+const port  = process.env.PORT
+app.listen(port, () => {
+    console.log(`Server started at http://${port}`);
+});
+}
